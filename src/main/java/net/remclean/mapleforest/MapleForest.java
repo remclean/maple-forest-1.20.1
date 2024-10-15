f1965b84443709d57135546895341c8edafe1134
remclean <remclean.osu@gmail.com>
2024-10-15 16:24:35 -0400

    First files after video 1

---
+++ src/main/java/net/remclean/mapleforest/MapleForest.java
@@ -0,0 +1,18 @@
+package net.remclean.mapleforest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MapleForest implements ModInitializer {
+	public static final String MOD_ID = "maple-forest";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/remclean/mapleforest/MapleForestClient.java
@@ -0,0 +1,10 @@
+package net.remclean.mapleforest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MapleForestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
