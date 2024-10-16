6276da498a99236e54669abfb64c6bc33b2da8b5
remclean <remclean.osu@gmail.com>
2024-10-15 21:26:17 -0400

    So close

--- src/main/java/net/remclean/mapleforest/MapleForest.java
+++ src/main/java/net/remclean/mapleforest/MapleForest.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
+import net.remclean.mapleforest.item.ModItems;
 
 public class MapleForest implements ModInitializer {
 	public static final String MOD_ID = "maple-forest";
--- src/main/java/net/remclean/mapleforest/item/ModItems.java
+++ src/main/java/net/remclean/mapleforest/item/ModItems.java
@@ -1,15 +1,20 @@
 package net.remclean.mapleforest.item;
 
-import net.remclean.mapleforest.MapleForest;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.remclean.mapleforest.MapleForest;
 
 public class ModItems {
     public static final Item MAPLE_SYRUP = registerItem("maplesyrup", new Item(new FabricItemSettings()));
 
-    public static void addItemsToIngredientItemsGroup(FabricItemGroupEntries entries) {
-        entries.add(MAPLE_SYRUP)
+    public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(MAPLE_SYRUP);
     }
 
     private static Item registerItem(String name, Item item) {
@@ -19,7 +24,7 @@ public class ModItems {
     public static void registerModItems() {
         MapleForest.LOGGER.info("Registering mod items for " + MapleForest.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToIngredientsItemsGroup)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToIngredientItemGroup);
     } 
 
 }
