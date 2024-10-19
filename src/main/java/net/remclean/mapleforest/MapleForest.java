255da3b8890de94bce3db379f8db84cf7bebf4ab
remclean <remclean.osu@gmail.com>
2024-10-19 08:49:27 -0400

    update

--- src/main/java/net/remclean/mapleforest/MapleForest.java
+++ src/main/java/net/remclean/mapleforest/MapleForest.java
@@ -2,7 +2,6 @@ package net.remclean.mapleforest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import net.fabricmc.api.ModInitializer;
 import net.remclean.mapleforest.item.ModItems;
 
