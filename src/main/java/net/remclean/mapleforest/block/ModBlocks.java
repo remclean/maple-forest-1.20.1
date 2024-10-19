32e0e3c1153fdb7bc5c9e2e67ea44a87b050e038
remclean <remclean.osu@gmail.com>
2024-10-19 14:06:28 -0400

    Fixes crash and added top/bottem texture to maple logs. Block states is next

--- src/main/java/net/remclean/mapleforest/block/ModBlocks.java
+++ src/main/java/net/remclean/mapleforest/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -15,7 +16,7 @@ public class ModBlocks {
     public static final Block MAPLE_PLANKS = registerBlock("maple_planks",
             new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS)));
     public static final Block MAPLE_LOG = registerBlock("maple_log",
-            new Block(FabricBlockSettings.copyOf(Blocks.OAK_LOG)));
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
