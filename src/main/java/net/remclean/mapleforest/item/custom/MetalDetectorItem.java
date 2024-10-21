8153d4716fcdb08ae032210f9e20e7023a202a62
remclean <remclean.osu@gmail.com>
2024-10-20 20:50:35 -0400

    Adds tooltip and crafting recipe for ore detector

--- src/main/java/net/remclean/mapleforest/item/custom/MetalDetectorItem.java
+++ src/main/java/net/remclean/mapleforest/item/custom/MetalDetectorItem.java
@@ -3,13 +3,19 @@ package net.remclean.mapleforest.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
@@ -52,5 +58,11 @@ public class MetalDetectorItem extends Item {
 
     private boolean isValuableBlock(BlockState state) {
         return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.maple-forest.ore_detector"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
